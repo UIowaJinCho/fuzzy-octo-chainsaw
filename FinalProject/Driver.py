1e6aa18e47fde1472350e8bfd04dc978a7cd7bc8
Jin Cho <ipdiss@naver.com>
2022-12-11 01:14:56 -0600

    TM loading working

--- FinalProject/Driver.py
+++ FinalProject/Driver.py
@@ -7,4 +7,7 @@ Author: Hyinjin Cho, Hieu Vu
 
 #Prompt user which TM to create and store the selectio.
 TM = PromptFilterInputMessage('Select the turing machine',[turingmachine for turingmachine in dir(tm) if not turingmachine.startswith('__')])
+
+#Load the turing machine and put it into variables
+States, Inputs, TapeSyms, Blank, Leftend, Trans, Start, Final = eval('tm.%s()'%TM[0])
 print(TM)
